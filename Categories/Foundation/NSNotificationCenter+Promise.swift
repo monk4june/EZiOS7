64ed18c71258fb9b3d567602528435c04d66a16f
Max Howell <mxcl@me.com>
2015-08-01 16:03:36 -0500

    2.1.3 — Generated Files

--- Categories/Foundation/NSNotificationCenter+Promise.swift
+++ Categories/Foundation/NSNotificationCenter+Promise.swift
@@ -15,13 +15,13 @@ import Foundation.NSNotification
 
 */
 extension NSNotificationCenter {
-    class func once(name: String) -> Promise<[NSObject: AnyObject]> {
+    public class func once(name: String) -> Promise<[NSObject: AnyObject]> {
         return once(name).then(on: zalgo) { (note: NSNotification) -> [NSObject: AnyObject] in
             return note.userInfo ?? [:]
         }
     }
 
-    class func once(name: String) -> Promise<NSNotification> {
+    public class func once(name: String) -> Promise<NSNotification> {
         return Promise { fulfill, _ in
             var id: AnyObject?
             id = NSNotificationCenter.defaultCenter().addObserverForName(name, object: nil, queue: PMKOperationQueue){ note in
